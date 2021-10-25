a42be6d0db3f56661f9be57ba30d20b0b57b2fee
Yuvis <yurii_s@bigmir.net>
2021-10-25 22:44:57 +0300

    homework for lesson Variables is done

---
+++ src/pro/sky/java/course1/homework/lesson/variables/Task1.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course1.homework.lesson.variables;
+
+public class Task1 {
+    public static void main(String[] args) {
+        byte a = 120;
+        short b = 10_000;
+        int c = 1_000_000;
+        long d = 1_000_000_000L;
+        float e = 1.25f;
+        double f = 2.8754;
+    }
+}
