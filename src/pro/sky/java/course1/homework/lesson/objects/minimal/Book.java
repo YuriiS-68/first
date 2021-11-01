39cbf4ae81a4253ca69ddf11f326a603c44aa8fd
Yuvis <yurii_s@bigmir.net>
2021-11-01 23:34:00 +0200

    homework for lesson Objects easy level

---
+++ src/pro/sky/java/course1/homework/lesson/objects/minimal/Book.java
@@ -0,0 +1,39 @@
+package pro.sky.java.course1.homework.lesson.objects.minimal;
+
+import pro.sky.java.course1.homework.lesson.objects.minimal.Author;
+
+public class Book {
+    private String nameBook;
+    private Author author;
+    private int yearPublication;
+
+    public Book(String nameBook, Author author, int yearPublication){
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public void setNameBook(String nameBook) {
+        this.nameBook = nameBook;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+}
