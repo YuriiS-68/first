a55e71d0de5b92cdc0620ae54a93695a3a49b301
Yuvis <yurii_s@bigmir.net>
2021-11-04 21:33:06 +0200

    homework for lesson Objects second part add easy level methods equals and hashCode

--- src/pro/sky/java/course1/homework/lesson/objects/minimal/Author.java
+++ src/pro/sky/java/course1/homework/lesson/objects/minimal/Author.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.homework.lesson.objects.minimal;
 
+import java.util.Objects;
+
 public class Author {
     private String firstName;
     private String lastName;
@@ -23,6 +25,19 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && lastName.equals(author.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 
     @Override
--- src/pro/sky/java/course1/homework/lesson/objects/minimal/Book.java
+++ src/pro/sky/java/course1/homework/lesson/objects/minimal/Book.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.homework.lesson.objects.minimal;
 
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private Author author;
@@ -33,6 +35,19 @@ public class Book {
 
     public void setYearPublication(int yearPublication) {
         this.yearPublication = yearPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublication == book.yearPublication && nameBook.equals(book.nameBook) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, yearPublication);
     }
 
     @Override
